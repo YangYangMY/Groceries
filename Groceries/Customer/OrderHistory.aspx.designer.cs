0feedec5983acb8345e8f98c400bcd0085e4915a
Yongyue0826 <109788606+Yongyue0826@users.noreply.github.com>
2023-04-06 00:14:35 +0800

    Upload Order Tracking

--- Groceries/Customer/OrderHistory.aspx.designer.cs
+++ Groceries/Customer/OrderHistory.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace Groceries.Customer
 
     public partial class OrderHistory
     {
-
-        /// <summary>
-        /// btnTrackProduct control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnTrackProduct;
     }
 }

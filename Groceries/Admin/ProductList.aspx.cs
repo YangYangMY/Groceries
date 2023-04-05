6183a615e673b4b0d4ba2fea4622d1460a51323e
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-04-05 10:55:24 +0800

    Update Product List

--- Groceries/Admin/ProductList.aspx.cs
+++ Groceries/Admin/ProductList.aspx.cs
@@ -13,5 +13,43 @@ namespace Groceries.Admin.Product
         {
 
         }
+
+        protected void ButtonRow1_Click(object sender, EventArgs e)
+        {
+            PanelEditProduct.Visible = true;
+            PanelProductTable.Visible = false;
+        }
+
+        protected void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            PanelEditProduct.Visible = false;
+            PanelProductTable.Visible = true;
+        }
+
+        protected void ButtonUpdate_Click(object sender, EventArgs e)
+        {
+            PanelEditProduct.Visible = false;
+            PanelProductTable.Visible = true;
+        }
+
+        protected void ButtonDelete1_Click(object sender, EventArgs e)
+        {
+            PanelConfirmDelete.Visible = true;
+        }
+
+        protected void ButtonCancelDeleteIcon_Click(object sender, EventArgs e)
+        {
+            PanelConfirmDelete.Visible = false;
+        }
+
+        protected void ButtonCancelDelete_Click(object sender, EventArgs e)
+        {
+            PanelConfirmDelete.Visible = false;
+        }
+
+        protected void ButtonConfirmDelete_Click(object sender, EventArgs e)
+        {
+            PanelConfirmDelete.Visible = false;
+        }
     }
 }

faadd6f374e6a1f1f9de67b345242ea79d5fbde8
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-04-06 08:13:24 +0800

    Update

--- Groceries/Customer/Profile.aspx.cs
+++ Groceries/Customer/Profile.aspx.cs
@@ -15,31 +15,6 @@ namespace Groceries.Customer
         StringBuilder table = new StringBuilder();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(!Page.IsPostBack)
-            {
-                SqlConnection con = new SqlConnection();
-                con.ConnectionString = ConfigurationManager.ConnectionStrings["GoceriesDatabaseEntities1"].ToString();
-                con.Open();
-                SqlCommand cmd = new SqlCommand();
-                cmd.CommandText = "Select * from [Customer]";
-                cmd.Connection = con;
-                SqlDataReader rd = cmd.ExecuteReader();
-                table.Append("<table border='1'>");
-                table.Append("<tr><th>Customer Id</th><th>Fname</th>");
-
-                if (rd.HasRows)
-                {
-                    while (rd.Read())
-                    {
-                        table.Append("<tr>");
-                        table.Append("<td>" + rd[0]+"</td>");
-                        table.Append("<td>" + rd[1]+"</td>");
-                        table.Append("</tr>");
-                    }
-                }
-                table.Append("</table>");
-                //PlaceHolder1.Controls.Add(new Literal { Text = table.ToString() });
-                rd.Close();
 
 
             }

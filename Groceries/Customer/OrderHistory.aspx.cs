1334eb58d89bfd5e49e40020ee1670f7300f32ba
Yongyue0826 <109788606+Yongyue0826@users.noreply.github.com>
2023-05-10 23:00:20 +0800

    pushnothing

--- Groceries/Customer/OrderHistory.aspx.cs
+++ Groceries/Customer/OrderHistory.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Groceries.Customer
 {
@@ -11,6 +13,27 @@ namespace Groceries.Customer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+
+            if (!IsPostBack) {
+
+                int customerID = Convert.ToInt32(Session["user"]); 
+                string strCon = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\GoceriesDatabase.mdf;Integrated Security=True;"; SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\GoceriesDatabase.mdf;Integrated Security=True;");
+                string query = String.Format("select OrderID, OrderDate from [Order] where CustomerID = {0}",customerID);
+
+
+                con = new SqlConnection(strCon);
+                SqlCommand command = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                RptOrderHistory.DataSource = reader;
+                RptOrderHistory.DataBind();
+                con.Close();
+
+               
+                
+
+            }
 
         }
     }
